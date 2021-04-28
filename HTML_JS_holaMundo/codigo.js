6db2ca831374290a44128c82987e3f0bf05197b9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-27 20:41:03 -0500

    Alert JS

--- HTML_JS_holaMundo/codigo.js
+++ HTML_JS_holaMundo/codigo.js
@@ -5,6 +5,9 @@
 //alert(nombre+" "+apellido+"\n"+edad+" años.");
 
 var peso;
+var pesoEnMarte;
+
 // prompt = Pedir datos, dato por defecto
-peso = prompt("¿Cuál es tu peso?: ", "100");
-alert(peso + " Kg.")
+peso = prompt("¿Cuál es tu peso?: ");
+peso = (peso/9.81)*(3.711)
+alert("Tu peso en marte es: "+peso)
