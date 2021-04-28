5e1d69314d8fe28aec7acc1ecadb7e1542f5f4cd
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-27 20:35:30 -0500

    alert en JS

--- HTML_JS_holaMundo/codigo.js
+++ HTML_JS_holaMundo/codigo.js
@@ -1,6 +1,10 @@
-var nombre = "Gustavo";
-var apellido = "Calzada";
-var edad = 21;
+//var nombre = "Gustavo";
+//var apellido = "Calzada";
+//var edad = 21;
 
-alert(nombre+" "+apellido+"\n"+edad+" años.");
+//alert(nombre+" "+apellido+"\n"+edad+" años.");
 
+var peso;
+// prompt = Pedir datos, dato por defecto
+peso = prompt("¿Cuál es tu peso?: ", "100");
+alert(peso + " Kg.")
