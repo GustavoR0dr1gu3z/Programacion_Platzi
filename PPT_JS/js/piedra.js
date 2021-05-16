83e315f18ed2209a61c99a274b10e2dcc3b8a183
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-15 22:11:48 -0500

    piedra papel o tijera en JS

--- PPT_JS/js/piedra.js
+++ PPT_JS/js/piedra.js
@@ -5,6 +5,16 @@ var tijera = 2;
 var opcionUsuario;
 var opcionMaquina;
 
-opcionUsuario = prompt("Piedra: 0\n Papel: 1\n Tijera: 2\n Opcion: ",0);
+opcionUsuario = prompt("Piedra: 0\nPapel: 1\nTijera: 2\n Opcion: ",0);
 
 alert("Ejegiste "+opcionUsuario);
+
+if(opcionUsuario == 0){
+    alert("Elegiste Piedra")
+}else if(opcionUsuario == 1){
+    alert("Elegiste Papel")
+}else if(opcionUsuario == 2){
+    alert("Elegiste Tijera")
+}else{
+    alert("Digite numero correcto")
+}
