2ff4af7e771b33f5484aaa465c97ad97958ddaef
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-16 21:55:43 -0500

    juego campo minado HTML JS

--- Juego_Campo_Minado_HTML_JS/matriz.js
+++ Juego_Campo_Minado_HTML_JS/matriz.js
@@ -24,3 +24,8 @@ x = prompt("Posicion X (Entre 0 y 2): ");
 y = prompt("Posicion Y (Entre 0 y 2): ");
 var posicion = campo[x][y];
 
+if (posicion != undefined){
+    alert("Posicion Correcta");
+}else{
+    boom();
+}
