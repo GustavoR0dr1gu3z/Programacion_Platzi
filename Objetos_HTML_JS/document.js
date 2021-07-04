bf768137322e33d24e16c33f8016a75589827a42
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-04 11:39:25 -0500

    Pruebas

--- Objetos_HTML_JS/document.js
+++ Objetos_HTML_JS/document.js
@@ -1,1 +1,11 @@
-alert("Funciona?");
+//Document Object Model
+
+
+// Document = Objeto que tiene html
+// Objeto = Funciones/Variables
+document.write("Hola");
+
+
+var pi = 3.141592654
+pi = Math.floor(pi)
+document.write(pi)
