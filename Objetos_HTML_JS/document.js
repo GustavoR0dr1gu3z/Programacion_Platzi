3dfa939372c08764151da5a03cadc9f6c3118a95
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-04 11:43:20 -0500

    Pruebas

--- Objetos_HTML_JS/document.js
+++ Objetos_HTML_JS/document.js
@@ -3,9 +3,13 @@
 
 // Document = Objeto que tiene html
 // Objeto = Funciones/Variables
-document.write("Hola");
+//document.write("Hola");
 
 
-var pi = 3.141592654
-pi = Math.floor(pi)
-document.write(pi)
+//var pi = 3.141592654
+//pi = Math.floor(pi)
+//document.write(pi)
+
+var maximo;
+maximo = Math.max(5,52,85,1,2,6,8,45);
+document.write(maximo)
